906640fd3525e0236a122cb5c5cfb415507ed7c6
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-07-26 12:18:08 -0300

    create pages and auth layout

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthGuard } from "./AuthGuard";
+import { Dashboard, Signin, SignUp } from "../pages";
+import { AuthLayout } from "../layouts/AuthLayout";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AuthGuard isPrivate={false} />}>
-          <Route path="/login" element={<h1>Login</h1>} />
-          <Route path="/register" element={<h1>Register</h1>} />
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Signin />} />
+            <Route path="/register" element={<SignUp />} />
+          </Route>
         </Route>
 
         <Route element={<AuthGuard isPrivate />}>
-          <Route index element={<h1>Dashboard</h1>} />
+          <Route index element={<Dashboard />} />
         </Route>
       </Routes>
     </BrowserRouter>
