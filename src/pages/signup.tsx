1e03dd75fc9b5dcdc4407031317581207ff646fe
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-07-28 18:40:55 -0300

    create signup hook form and validate fields

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { useSignupForm } from "../hooks/useSignupForm";
 
 export function SignUp() {
+  const { handleSubmit, register, errors } = useSignupForm();
+
   return (
     <>
       <header className="flex flex-col items-center gap-4">
@@ -11,7 +14,9 @@ export function SignUp() {
         </h1>
 
         <p className="space-x-2">
-          <span className="tracking[-0.5] text-gray-700">Já possui uma conta ?</span>
+          <span className="tracking[-0.5] text-gray-700">
+            Já possui uma conta ?
+          </span>
 
           <Link
             to="/signin"
@@ -22,10 +27,26 @@ export function SignUp() {
         </p>
       </header>
 
-      <form className="mt-[60px] flex flex-col gap-4">
-        <Input placeholder="Nome" name="name" />
-        <Input type="email" placeholder="E-mail" name="email" />
-        <Input type="password" placeholder="Senha" name="password" />
+      <form className="mt-[60px] flex flex-col gap-4" onSubmit={handleSubmit}>
+        <Input
+          placeholder="Nome"
+          {...register("name")}
+          error={errors.name?.message}
+        />
+
+        <Input
+          type="email"
+          placeholder="E-mail"
+          {...register("email")}
+          error={errors.email?.message}
+        />
+
+        <Input
+          type="password"
+          placeholder="Senha"
+          {...register("password")}
+          error={errors.password?.message}
+        />
         <Button type="submit" className="mt-2">
           Criar conta
         </Button>
