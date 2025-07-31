69e6c87dbff63686c5245f7cb47d2e90ee986b11
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-07-30 21:17:11 -0300

    launch dashboard screen

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -1,13 +1,23 @@
-import { useAuth } from '@/app/hooks/useAuth';
-import { Button } from "@/view/components";
+import { Logo } from "@/view/components";
+import { UserMenu } from "@/view/components/UserMenu";
+import { Accounts, Transactions } from "./components";
 
 export function Dashboard() {
-  const { signout } = useAuth();
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="flex h-full w-full flex-col gap-4 px-8 pt-8 pb-8">
+      <header className="flex h-12 items-center justify-between">
+        <Logo className="h-6 text-teal-900" />
+        <UserMenu />
+      </header>
 
-      <Button onClick={signout}>Sair</Button>
+      <main className="flex flex-1 gap-4">
+        <div className="w-1/2">
+          <Accounts />
+        </div>
+        <div className="w-1/2">
+          <Transactions />
+        </div>
+      </main>
     </div>
   );
 }
