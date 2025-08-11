9ebc526ca6b1ee19a920ee80d3922a3ed87e9494
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-08-11 14:50:07 -0300

    create modal with radix and config animation

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -1,8 +1,6 @@
-import { Logo } from "@/view/components";
-import { UserMenu } from "@/view/components/UserMenu";
-import { Accounts, Transactions } from "./components";
+import { Logo, Modal, UserMenu } from "@/view/components";
+import { Accounts, Fab, Transactions } from "./components";
 import { DashboardProvider } from "./context/DashboardProvider";
-import { Fab } from "./components/Fab";
 
 export function Dashboard() {
   return (
@@ -23,6 +21,8 @@ export function Dashboard() {
         </main>
 
         <Fab />
+
+        <Modal />
       </div>
     </DashboardProvider>
   );
