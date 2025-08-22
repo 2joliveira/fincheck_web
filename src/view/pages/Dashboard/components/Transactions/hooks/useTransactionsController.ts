c7c3d19ac17258b217efe90508c022fb0bab9774
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-08-22 10:35:10 -0300

    create get all transactions service

--- src/view/pages/Dashboard/components/Transactions/hooks/useTransactionsController.ts
+++ src/view/pages/Dashboard/components/Transactions/hooks/useTransactionsController.ts
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useDashboard } from "../../../context/DashboardContext";
+import { useTransactions } from "@/app/hooks/useTransactions";
 
 export function useTransactionsController() {
   const { areValuesVisible } = useDashboard();
@@ -11,6 +12,10 @@ export function useTransactionsController() {
 
   const [isFiltersModalOpen, setIsFiltersModalOpen] = useState(false);
 
+  const { transactions } = useTransactions();
+
+  console.log({ transactions });
+  
   function handleOpenFiltersModal() {
     setIsFiltersModalOpen(true);
   }
